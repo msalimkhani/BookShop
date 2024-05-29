91e592851b9d587c41fe86d3ca05c807b02b04d1
msalimkhani <m.salimkhani@iau.ir>
2024-05-30 01:16:17 +0330

    Update repo

--- BookShop/BookShop/BookShopApp.DataLayer/obj/Debug/net8.0/BookShopApp.DataLayer.AssemblyInfo.cs
+++ BookShop/BookShop/BookShopApp.DataLayer/obj/Debug/net8.0/BookShopApp.DataLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShopApp.DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df16007dcb8fb51a75dab37ceae47387f06febdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShopApp.DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShopApp.DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BookShop/BookShop/BookShopApp/obj/Debug/net8.0/BookShopApp.AssemblyInfo.cs
+++ BookShop/BookShop/BookShopApp/obj/Debug/net8.0/BookShopApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df16007dcb8fb51a75dab37ceae47387f06febdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
